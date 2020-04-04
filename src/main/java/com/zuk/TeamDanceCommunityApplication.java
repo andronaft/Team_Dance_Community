194f06564d6ff87f1bbe812eeb5d940a7e5d1db3
andronaft <andronaft99@gmail.com>
2020-04-04 17:24:51 +0300

    create inital structure

---
+++ src/main/java/com/zuk/TeamDanceCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.zuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDanceCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDanceCommunityApplication.class, args);
+	}
+
+}
