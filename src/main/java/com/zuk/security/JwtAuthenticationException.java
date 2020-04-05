f1663136e8ea8de0b98a6532a939f2a0b5bd06fb
andronaft <andronaft99@gmail.com>
2020-04-06 02:43:16 +0300

    Add Jwt. Add SecurityConfig

---
+++ src/main/java/com/zuk/security/JwtAuthenticationException.java
@@ -0,0 +1,14 @@
+package com.zuk.security;
+
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public JwtAuthenticationException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/zuk/security/JwtUserFactory.java
@@ -0,0 +1,38 @@
+package com.zuk.security;
+
+import com.zuk.model.Role;
+import com.zuk.model.Status;
+import com.zuk.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class JwtUserFactory {
+
+    public JwtUserFactory() {
+    }
+
+    public static JwtUser create(User user) {
+        return new JwtUser(
+                user.getId(),
+                user.getUsername(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                mapToGrantedAuthorities(new ArrayList<>(user.getRoles())),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getUpdated()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities(List<Role> userRoles) {
+        return userRoles.stream()
+                .map(role ->
+                        new SimpleGrantedAuthority(role.getName())
+                ).collect(Collectors.toList());
+    }
+}
