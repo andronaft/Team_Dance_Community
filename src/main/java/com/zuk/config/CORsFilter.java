ce15c16e9f38db1f67bd59e991d7b26ce7db59ff
andronaft <andronaft99@gmail.com>
2020-04-12 23:00:29 +0300

    Change CORS 0.2

--- src/main/java/com/zuk/config/CORsFilter.java
+++ src/main/java/com/zuk/config/CORsFilter.java
@@ -16,6 +16,7 @@ public class CORsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
