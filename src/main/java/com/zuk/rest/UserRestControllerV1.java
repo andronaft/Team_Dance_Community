3896a412907b593235ced05758feb6bef1fc2ef7
andronaft <andronaft99@gmail.com>
2020-04-16 20:31:50 +0300

    Change users get/update Profile

--- src/main/java/com/zuk/rest/UserRestControllerV1.java
+++ src/main/java/com/zuk/rest/UserRestControllerV1.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -45,62 +46,6 @@ public class UserRestControllerV1 {
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping(value = "getProfile/")
-    public ResponseEntity getUserProfileById(@RequestBody UserDto userDto,@RequestHeader("Authorization") String token){
 
 
-        System.out.println(jwtTokenProvider.getUsername(token.substring(7)));
-        if(jwtTokenProvider.getUsername(token.substring(7)).equals(userDto.getUsername())){
-            System.out.println("1");
-            User user = userService.findByUsername(userDto.getUsername());
-            System.out.println("2");
-            UserProfile userProfile = userProfileService.findById(user.getId());
-            if(userProfile == null){
-                throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
-            }
-            System.out.println("3");
-            Map<Object, Object> response = new HashMap<>();
-            response.put("user_id",userProfile.getUserId());
-            response.put("mobile",userProfile.getMobile());
-            response.put("social",userProfile.getSocial());
-            response.put("about",userProfile.getAbout());
-            response.put("img_url",userProfile.getRating());
-            response.put("rating",userProfile.getRating());
-            response.put("level",userProfile.getLevel());
-            return new ResponseEntity(response, HttpStatus.OK);
-        }
-
-        throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
-
-
-    }
-    @PostMapping(value = "updateProfile/")
-    public ResponseEntity updateProfile(@RequestBody UserDto userDto,@RequestHeader("Authorization") String token,@RequestBody UserProfileDto userProfileDto){
-
-
-        System.out.println(jwtTokenProvider.getUsername(token.substring(7)));
-        if(jwtTokenProvider.getUsername(token.substring(7)).equals(userDto.getUsername())){
-            System.out.println("1");
-            User user = userService.findByUsername(userDto.getUsername());
-            System.out.println("2");
-            UserProfile userProfile = userProfileService.findById(user.getId());
-            if(userProfile == null){
-                throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
-            }
-            System.out.println("3");
-            Map<Object, Object> response = new HashMap<>();
-            response.put("user_id",userProfile.getUserId());
-            response.put("mobile",userProfile.getMobile());
-            response.put("social",userProfile.getSocial());
-            response.put("about",userProfile.getAbout());
-            response.put("img_url",userProfile.getRating());
-            response.put("rating",userProfile.getRating());
-            response.put("level",userProfile.getLevel());
-            return new ResponseEntity(response, HttpStatus.OK);
-        }
-
-        throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
-
-
-    }
 }
--- src/main/java/com/zuk/rest/user/ProfileControllerV1.java
+++ src/main/java/com/zuk/rest/user/ProfileControllerV1.java
@@ -35,14 +35,12 @@ public class ProfileControllerV1 {
     }
 
     @PostMapping(value = "getProfile/")
-    public ResponseEntity getUserProfileById(@RequestBody UserDto userDto, @RequestHeader("Authorization") String token){
+    public ResponseEntity getUserProfileById( @RequestHeader("Authorization") String token){
 
         try {
-            System.out.println(jwtTokenProvider.getUsername(token.substring(7)));
-            if (jwtTokenProvider.getUsername(token.substring(7)).equals(userDto.getUsername())) {
-                System.out.println("1");
-                User user = userService.findByUsername(userDto.getUsername());
-                System.out.println("2");
+
+                User user = userService.findByUsername(jwtTokenProvider.getUsername(token.substring(7)));
+
                 UserProfile userProfile = userProfileService.findById(user.getId());
                 if (userProfile == null) {
                     throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
@@ -57,9 +55,8 @@ public class ProfileControllerV1 {
                 response.put("rating", userProfile.getRating());
                 response.put("level", userProfile.getLevel());
                 return new ResponseEntity(response, HttpStatus.OK);
-            }
 
-            throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
+
         } catch (AuthenticationException e) {
             //throw  new UsernameNotFoundException("User  not found");
             throw new BadCredentialsException("Invalid username or password");
@@ -70,11 +67,8 @@ public class ProfileControllerV1 {
     public ResponseEntity updateProfile(@RequestHeader("Authorization") String token,@RequestBody UserProfileDto userProfileDto){
 
         try {
-            System.out.println(jwtTokenProvider.getUsername(token.substring(7)));
-            if (jwtTokenProvider.getUsername(token.substring(7)).equals(userProfileDto.getUsername())) {
-                System.out.println("1");
-                User user = userService.findByUsername(userProfileDto.getUsername());
-                System.out.println("2");
+
+                User user = userService.findByUsername(jwtTokenProvider.getUsername(token.substring(7)));
                 UserProfile userProfile = userProfileDto.toUserProfile();
                 userProfile.setUserId(user.getId());
                 userProfile.setCreated(user.getCreated());
@@ -93,9 +87,7 @@ public class ProfileControllerV1 {
                 response.put("rating", userProfileUpdate.getRating());
                 response.put("level", userProfileUpdate.getLevel());
                 return new ResponseEntity(response, HttpStatus.OK);
-            }
 
-            throw new BadCredentialsException("Invalid username");//return new ResponseEntity(HttpStatus.NO_CONTENT);
 
         }catch (AuthenticationException e) {
             //throw  new UsernameNotFoundException("User  not found");
