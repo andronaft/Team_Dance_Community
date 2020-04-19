77be135eb24e403b4165a60371be417e25303d82
andronaft <andronaft99@gmail.com>
2020-04-19 18:35:08 +0300

    add AdminGroupControllerV1

---
+++ src/main/java/com/zuk/rest/admin/training/AdminGroupTrainingControllerV1.java
@@ -0,0 +1,9 @@
+package com.zuk.rest.admin.training;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/admin/training/groupTraining")
+public class AdminGroupTrainingControllerV1 {
+}
--- src/main/java/com/zuk/rest/training/GroupTrainingControllerV1.java
+++ src/main/java/com/zuk/rest/training/GroupTrainingControllerV1.java
@@ -3,16 +3,15 @@ package com.zuk.rest.training;
 
 import com.zuk.dto.branch.BranchDto;
 import com.zuk.dto.training.GroupTrainingDto;
+import com.zuk.model.WeekDay;
 import com.zuk.service.GroupTrainingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/v1/training/")
+@RequestMapping(value = "/api/v1/training/groupTraining/")
 public class GroupTrainingControllerV1 {
     private final GroupTrainingService groupTrainingService;
 
@@ -21,15 +20,26 @@ public class GroupTrainingControllerV1 {
         this.groupTrainingService = groupTrainingService;
     }
 
-    @GetMapping(value = "1")
-    public ResponseEntity findById(){
-        long id = 1;
+    @GetMapping(value = "{id}")
+    public ResponseEntity findById(@PathVariable(name = "id") Long id){
+
         return new ResponseEntity<>(GroupTrainingDto.fromGroupTraining(groupTrainingService.findById(id)), HttpStatus.OK);
     }
 
    @GetMapping(value = "")
-    public ResponseEntity finadAll(){
+    public ResponseEntity findAll(){
 
        return new ResponseEntity<>(GroupTrainingDto.fromArrayGroupTraining(groupTrainingService.findAll()), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "findByDay")
+    public ResponseEntity findByDay(@RequestBody GroupTrainingDto groupTrainingDto){
+
+        return new ResponseEntity<>(GroupTrainingDto.fromArrayGroupTraining(groupTrainingService.findByDay(groupTrainingDto.getWeekDay())), HttpStatus.OK);
+    }
+    @GetMapping(value = "findByBranch")
+    public ResponseEntity findByBranch(@RequestBody GroupTrainingDto groupTrainingDto){
+        System.out.println(groupTrainingDto.getBranchId());
+        return new ResponseEntity<>(GroupTrainingDto.fromArrayGroupTraining(groupTrainingService.findByBranch(groupTrainingDto.getBranchId())), HttpStatus.OK);
     }
 }
